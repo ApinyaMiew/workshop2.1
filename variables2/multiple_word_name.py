653d353103f7ec91de0fce5fae5ca4a573d4bcc8
Apinya_LENOVO <apinya.chaiw@ku.th>
2021-01-10 11:10:18 +0700

    add many_value, multiple_word_name, name

---
+++ variables2/multiple_word_name.py
@@ -0,0 +1,8 @@
+# Camal Case (เขียนเหมือนหลังอูฐ)
+myVariableName = "Apinya Chaiwong"
+
+# Pascal Case (ขึ้นต้นคำตัวใหญ่หมด)
+MyVariableName = "Apinya Chaiwong"
+
+# Snake Case (ตัวเล็กหมด แล้วระหว่างคำมี "_ " คั่น) (คนส่วนใหญ่ใช้ประกาศตัวแปร)
+my_Variable_name = "Apinya Chaiwong"
---
+++ variables2/name.py
@@ -0,0 +1,17 @@
+myvar = "Miew"
+my_var = "Miew"
+_my_var_ "Miew"
+myVar = "Miew"
+MYVAR = "Miew"
+myvar2 = "Miew"
+ 
+#ไม่สามารถประกาศได้
+2myvar = "Miew"
+my-var = "Miew"
+my var = "Miew"
+
+
+
+#PI = 3.14
+#pi = 3.14
+#สร้างตัวแปรพิมใหญ๋ทั้งหมดหมายความว่า ไม่สามารถเปลี่ยนแปลงค่าได้
