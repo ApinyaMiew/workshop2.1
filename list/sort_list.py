68285c6230e87650e215ff458f2c8f6dcfce1a74
Apinya_LENOVO <apinya.chaiw@ku.th>
2021-01-17 10:00:05 +0700

    add python_lists

---
+++ list/sort_list.py
@@ -0,0 +1,9 @@
+# EXAMPLE 1
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()  # น้อยไปมาก
+print(thislist)  # Output: [ 23,  50,  65,  82,  100]
+
+# EXAMPLE 2
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse=True)  # มากไปน้อย
+print(thislist)  # Output: [ 100,  82,  65,  50,  23]
