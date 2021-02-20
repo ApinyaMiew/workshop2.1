ce3686cced392b7982a301eb8eaebeea71eeb231
Apinya_LENOVO <apinya.chaiw@ku.th>
2021-02-20 12:09:36 +0700

    push เรื่อง class, function, modules and json

---
+++ module/class/run_class.py
@@ -0,0 +1,12 @@
+import classPerson
+
+name = "Apinya Chaiwong"
+age = 21
+address = "London"
+
+person = classPerson.Person(name, age, address)
+
+print(person.get_name())
+print(person.get_age)
+print(person.get_address())
+print(person.get_info())
---
+++ module/variables/variables.py
@@ -0,0 +1,2 @@
+person = {"name": "Miew", "age": 21, "country": "Bangkok"}
+person = {"name": "Por", "age": 24, "country": "Bangkok"}
